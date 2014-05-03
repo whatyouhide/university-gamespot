aeeebecef92fbebc9adad1d4e1629a1e7dd8a4da
Andrea Leopardi <an.leopardi@gmail.com>
2014-05-03 20:00:01 +0200

    Add a couple of utility functions
    These function are aimed at detecting the current request type.

--- app/functions.php
+++ app/functions.php
@@ -5,4 +5,18 @@ function redirect($location) {
   header("Location: " . SITE_ROOT . $location);
   die();
 }
+
+
+// Detect the type of the current request.
+function request_type() {
+  return $_SERVER['REQUEST_METHOD'];
+}
+
+// Some helper functions which detect various types of requests.
+function is_get_request() {
+  return request_type() == 'GET';
+}
+function is_post_request() {
+  return request_type() == 'POST';
+}
 ?>
