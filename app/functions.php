0ae41e3c0f566fa138b223a74c2bdf55988827e0
Andrea Leopardi <an.leopardi@gmail.com>
2014-05-11 19:38:27 +0200

    Add a bunch of functions

--- app/functions.php
+++ app/functions.php
@@ -6,6 +6,12 @@ function redirect($location) {
   die();
 }
 
+// Redirect to a page passing some data in the GET request.
+function redirect_with_data($location, $data) {
+  $new_url = $location . '?' . http_build_query($data);
+  redirect($new_url);
+}
+
 
 // Detect the type of the current request.
 function request_type() {
@@ -18,5 +24,15 @@ function is_get_request() {
 }
 function is_post_request() {
   return request_type() == 'POST';
+}
+
+// Check if a user is currently signed in.
+function is_signed_in() {
+  return $_SESSION['user'];
+}
+
+// Create a MySQL friendly timestamp of the current time.
+function mysql_timestamp() {
+  return date('Y-m-d H:i:s', time());
 }
 ?>
