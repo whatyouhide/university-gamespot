4d881e86ea5c03c539ffd4cd4ca75df59d309efe
Andrea Leopardi <an.leopardi@gmail.com>
2014-08-15 13:27:06 +0200

    Fix failing tests

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,9 +1,6 @@
 <?php
-// This is global bootstrap for autoloading
-// require_once(dirname(__FILE__) . '/../app/setup.php');
-
-require_once(dirname(__FILE__) . '/../app/model.class.php');
-require_once(dirname(__FILE__) . '/../app/db.class.php');
-require_once(dirname(__FILE__) . '/../app/functions.php');
+require_once(dirname(__FILE__) . '/../app/classes/model.class.php');
+require_once(dirname(__FILE__) . '/../app/classes/db.class.php');
 require_once(dirname(__FILE__) . '/../app/models/user.class.php');
+require_once(dirname(__FILE__) . '/../app/functions.php');
 ?>
