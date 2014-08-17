f3f8f0c1b66c20dc28be9adb61496b647769165b
Andrea Leopardi <an.leopardi@gmail.com>
2014-08-17 19:42:52 +0200

    Update the test bootstrap

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -3,5 +3,6 @@ require_once(dirname(__FILE__) . '/../app/classes/model.class.php');
 require_once(dirname(__FILE__) . '/../app/classes/db.class.php');
 require_once(dirname(__FILE__) . '/../app/models/user.class.php');
 require_once(dirname(__FILE__) . '/../app/models/game.class.php');
+require_once(dirname(__FILE__) . '/../app/models/upload.class.php');
 require_once(dirname(__FILE__) . '/../app/functions.php');
 ?>
