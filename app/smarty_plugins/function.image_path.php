36801b24423ddf88957977c8098fff10e0686195
Andrea Leopardi <an.leopardi@gmail.com>
2014-08-20 15:48:10 +0200

    Fix minor bugs in the Smarty plugins

--- app/smarty_plugins/function.image_path.php
+++ app/smarty_plugins/function.image_path.php
@@ -12,7 +12,7 @@ function smarty_function_image_path($params, $smarty) {
 
   // Return a regular image tag if there's a `src` attribute.
   if (isset($params['src'])) {
-    $src = $asset_images_path . $param['src'];
+    $src = $asset_images_path . $params['src'];
   } else if (isset($params['image'])) {
     $src = $uploads_path . '/' . $params['image']->url;
   } else if (isset($params['default'])) {
--- app/smarty_plugins/function.url.php
+++ app/smarty_plugins/function.url.php
@@ -10,9 +10,11 @@ function smarty_function_url($params, $smarty) {
   $base_url = $params['to'];
   unset($params['to']);
 
-  $href = $smarty->getTemplateVars('site_root');
-  $href .= '/' . $base_url;
-  $href .= '?' . http_build_query($params);
+  $href = $smarty->getTemplateVars('site_root') . $base_url;
+
+  if (!empty($params)) {
+    $href .= '?' . http_build_query($params);
+  }
 
   return $href;
 }
