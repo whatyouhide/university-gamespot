040264cb21e2a6594711d8f25345f3aa45844299
Andrea Leopardi <an.leopardi@gmail.com>
2014-08-22 02:04:33 +0200

    Add some comments to the Console model

--- app/models/console.php
+++ app/models/console.php
@@ -6,23 +6,40 @@
 class Console extends Model {
   public static $table_name = 'consoles';
 
+  /**
+   * {@inheritdoc}
+   * Also fetch the image associated with this console.
+   */
   public function __construct($attributes) {
     parent::__construct($attributes);
     $this->image = $this->associated_image();
   }
 
+  /**
+   * Update the image of this console to a given Upload instance. Delete the
+   * image that was asssociated with this record first (if there was one).
+   * @param Upload $upload The new image for this record.
+   */
   public function update_image($upload) {
     $this->delete_image();
     $upload->update(['console_image_id' => $this->id]);
     $this->image = $upload;
   }
 
+  /**
+   * Delete the image associated with this record. If there weren't any, do
+   * nothing.
+   */
   public function delete_image() {
     if (!$this->image) { return; }
     $this->image->destroy();
     $this->image = null;
   }
 
+  /**
+   * Fetch the image associated with this record.
+   * @return Upload
+   */
   private function associated_image() {
     return Upload::find_by_attribute('console_image_id', $this->id);
   }
